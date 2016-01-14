5748b554bf77347a2046349a87af668ef6f3d3eb
yzernik <yzernik@gmail.com>
2016-01-14 00:10:09 -0800

    improved bob question

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,15 +1,13 @@
 
 
 class Bob {
-  def hey(prompt: String) = {
-    val letters = prompt.filter(_.isLetter)
-    if (!letters.isEmpty && letters.forall(_.isUpper))
+  def hey(prompt: String) =
+    if (prompt.exists(_.isLetter) && prompt.toUpperCase == prompt)
       "Whoa, chill out!"
     else if (prompt.endsWith("?"))
       "Sure."
-    else if (prompt.filter(_.isLetterOrDigit).isEmpty)
+    else if (!prompt.exists(_.isLetterOrDigit))
       "Fine. Be that way!"
     else
       "Whatever."
-  }
 }
