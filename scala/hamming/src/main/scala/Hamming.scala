2b4344bf7277622d10fae0ba3ce07403626ff2f2
yzernik <yzernik@gmail.com>
2016-01-14 01:13:03 -0800

    simplify hamming

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -4,7 +4,6 @@ object Hamming {
   def compute(first: String, second: String) = {
     require(first.length == second.length)
     first.zip(second)
-      .filter { case (x, y) => x != y }
-      .length
+      .count { case (x, y) => x != y }
   }
 }
